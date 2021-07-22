ce38902d991db7ae55dbf1e6d2ea532bdf2a9267
swathinagarajan <87808434+swathinagarajan@users.noreply.github.com>
2021-07-22 16:32:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	void leftRotate(int arr[], int d, int n)
+    {
+        int temp[] = new int[d];
+        for (int i = 0; i < d; i++)
+            temp[i] = arr[i];
+ 
+        for (int i = d; i < n; i++) {
+            arr[i - d] = arr[i];
+        }
+ 
+        for (int i = 0; i < d; i++) {
+            arr[i + n - d] = temp[i];
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        int[] arr = { 1, 2, 3, 4, 5 };
+        main.leftRotate(arr, 2, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
